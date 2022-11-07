7c2c7356acfead9b8cc48df32b25c588a8759e70
alinbrg <alina.bregvadze@gmail.com>
2022-11-07 18:57:48 +0400

    initial files

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+const startTimeout = document.querySelector(".start-timeout");
+const endTimeout = document.querySelector(".end-timeout");
+const startInterval = document.querySelector(".start-interval");
+const endInterval = document.querySelector(".end-interval");
+
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
